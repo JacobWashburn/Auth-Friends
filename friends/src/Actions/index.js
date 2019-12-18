b7866bb5d51a46a1cbdcde75b5cfb62460113af9
Jacob Washburn <jacobrwashburn@gmail.com>
2019-12-17 23:41:51 -0700

    index.js: all actions exported here

---
+++ friends/src/Actions/index.js
@@ -0,0 +1,3 @@
+export {getAllFriends, GET_FRIENDS_START, GET_FRIENDS_SUCCESS, GET_FRIENDS_FAILURE} from './GetFriends';
+export {postLogin, POST_LOGIN_START, POST_LOGIN_SUCCESS, POST_LOGIN_FAILURE} from './PostLogin';
+export {postFriend,POST_FRIEND_START,POST_FRIEND_SUCCESS,POST_FRIEND_FAILURE} from './PostFriend'
